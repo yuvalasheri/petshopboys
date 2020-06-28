b28514581874112c6f8901b9386469748be54c1f
Yuval Asheri <yuvalasheri@gmail.com>
2020-06-28 12:44:05 +0300

    ini changes

--- INI/SAMPLE_startdev.bat
+++ INI/SAMPLE_startdev.bat
@@ -1,1 +1,1 @@
-c:\magicxpa46\mgxpastudio.exe /ini=c:\magicxpa46\magic.ini @INI\myfirstgitproject.ini @yuval.ini
+start c:\magicxpa46\mgxpastudio.exe /ini=c:\magicxpa46\magic.ini @INI\project.ini @developer.ini
