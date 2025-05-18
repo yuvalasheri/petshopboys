49fef135507926a27ee02cf4eef7262c8cdbd529
yuvalasheri <yuvala@magicsoftware.com>
2025-05-18 18:24:31 +0300

    Update my-custom-script.bat

--- my-custom-script.bat
+++ my-custom-script.bat
@@ -1,1 +1,3 @@
 dir > a.out
+cd C:\MagicProjects\checkpetshopboys\Checker
+"c:\magicxpa411\Mgxpastudioservices.exe"  /AutomaticProcessingSequenceFile=C:\\MagicProjects\\checkpetshopboys\\Checker\\petshopboyscheckproject.txt
