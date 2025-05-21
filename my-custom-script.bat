c84038a00d6c84306ec56d719f1623e2e4272beb
yuvalasheri <yuvala@magicsoftware.com>
2025-05-21 18:31:40 +0300

    Update my-custom-script.bat

--- my-custom-script.bat
+++ my-custom-script.bat
@@ -1,3 +1,16 @@
-dir > a.out
+
 cd C:\MagicProjects\checkpetshopboys\Checker
 "c:\magicxpa411\Mgxpastudioservices.exe"  /AutomaticProcessingSequenceFile=C:\\MagicProjects\\checkpetshopboys\\Checker\\petshopboyscheckproject.txt
+
+findstr "Errors" "petshopboysCheckerOutput.txt" >ErrorOutput.txt
+echo "Errors"                  : 0 ,> NoErrors.txt
+
+fc ErrorOutput.txt  NoErrors.txt > nul
+if errorlevel 1 goto error
+
+exit 0
+
+:error
+
+echo "Did not pass checker"
+exit 999
